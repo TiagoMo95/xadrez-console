5ee1e473af8ba053b82b25ef2bdf0612ef0b4a52
Tiago Morais <tiagomo1995@gmail.com>
2024-11-01 16:42:58 -0300

    Melhorando a implementacao do metodo ImprimirTabuleiro

--- Program.cs
+++ Program.cs
@@ -13,8 +13,10 @@ class Program
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
-            tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 4));
+
+            tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(3, 5));
 
             Tela.ImprimirTabuleiro(tab);
         }
