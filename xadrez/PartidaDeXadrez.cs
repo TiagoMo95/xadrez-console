d92fd269b0af5a56c7bf4087aec60c8e59e27b7e
Tiago Morais <tiagomo1995@gmail.com>
2024-11-02 10:48:42 -0300

    Fazendo os primeiros testes de movimentacao

---
+++ xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class PartidaDeXadrez
+    {
+        public Tabuleiro Tab { get; private set; }
+        private int turno;
+        private Cor jogadorAtual;
+        public bool Terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            Tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+            Terminada = false;
+            ColocarPecas();
+            
+        }
+
+        public void ExecutaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = Tab.RetirarPeca(origem);
+            p.IncrementarQteMovimentos();
+            Peca pecaCapturada = Tab.RetirarPeca(destino);
+            Tab.ColocarPeca(p, destino);
+        }
+
+        private void ColocarPecas()
+        {
+            Tab.ColocarPeca(new Torre(Cor.Branca, Tab), new PosicaoXadrez('c', 1).ToPosicao());
+        }
+    }
+}
